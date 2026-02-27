2f99df4068a85f10a66c61eb3bd40f3f0b81dbc7
fluttertutorialin <kamal.lakhani56@gmail.com>
2026-02-27 22:36:26 +0530

    Latest koin library add

--- composeApp/src/androidMain/kotlin/ke/don/scope_exampl/ScopeApplication.kt
+++ composeApp/src/androidMain/kotlin/ke/don/scope_exampl/ScopeApplication.kt
@@ -1,10 +1,10 @@
 package ke.don.scope_exampl
 
 import android.app.Application
+import org.koin.core.component.KoinComponent
 
-class ScopeApplication: Application() {
+class ScopeApplication: Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
-
     }
 }
